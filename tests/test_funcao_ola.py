5643cdd7e84961072f2c2d47ab2a79c3623decdd
Felipe Sebben <felipesebben@yahoo.com.br>
2024-03-06 16:04:03 -0300

    tests: update tests

--- tests/test_funcao_ola.py
+++ tests/test_funcao_ola.py
@@ -1,4 +1,6 @@
 from app.funcao import funcao_ola_pessoal
 
 def test_funcao_ola_pessoal():
-    assert funcao_ola_pessoal() == "Olá pessoal!"
+    saida = funcao_ola_pessoal()
+    gabarito = "Olá pessoal!"   
+    assert saida == gabarito
